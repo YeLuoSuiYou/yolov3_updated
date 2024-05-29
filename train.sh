cb483a89f95fa5be08cff04cba0d209b70f92a94
YeLuoSuiYou <ftzilvye@mail.scut.edu.cn>
2024-05-29 19:00:27 +0800

    update dataset yaml

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node 4 --master_port 2566 train.py --img 640 --batch 96 --data data/CCTSDB2021.yaml --weights yolov3.pt --cfg models/yolov3.yaml --device 0,1,2,3 --epochs 500 --workers 16 --optimizer AdamW
+python -m torch.distributed.run --nproc_per_node 4 --master_port 2566 train.py --img 640 --batch 92 --data data/lisa.yaml --weights yolov3.pt --cfg models/yolov3.yaml --device 0,1,2,3 --epochs 500 --workers 16 --optimizer AdamW
