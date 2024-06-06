23e79a85b57a0f4f837a58b16aa633c2533a6048
YeLuoSuiYou <ftzilvye@mail.scut.edu.cn>
2024-06-07 01:34:36 +0800

    commit from yzl

---
+++ train.sh
@@ -0,0 +1,2 @@
+export OMP_NUM_THREADS=8
+python -m torch.distributed.run --nproc_per_node 4 --master_port 2566 train.py --img 640 --batch 32 --data data/lisa.yaml --weights yolov5x.pt --cfg models/nihao.yaml --device 0,1,2,3 --epochs 500 --workers 16 --optimizer AdamW
